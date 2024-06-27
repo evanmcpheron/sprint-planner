8950d70195910b90c245258541d584e1815084fc
Brandon George <brandonkgeorge@gmail.com>
2024-06-26 21:41:38 -0500

    feat: improve and secure docker image
    slimmed docker file down by using alpine and removing unused node packages
    changed node to run in production mode
    changed docker image to not run as root
    changed docker image to have secure file and folder permissions
    optimized the build process for the image
    slimmed the layers for the image

--- client/src/index.js
+++ client/src/index.js
@@ -1,37 +1,37 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./style/import.scss";
-import { Provider } from "react-redux";
-import store from "./store";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { SocketContext, socket } from "./context/socket";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import GenerateRoom from "./components/GenerateRoom";
-import AddUser from "./components/AddUser";
-import { Box, createTheme, ThemeProvider } from "@mui/material";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './style/import.scss'
+import { Provider } from 'react-redux'
+import store from './store'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { SocketContext, socket } from './context/socket'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import GenerateRoom from './components/GenerateRoom'
+import AddUser from './components/AddUser'
+import { Box, createTheme, ThemeProvider } from '@mui/material'
 
 const darkTheme = createTheme({
   palette: {
-    mode: "dark",
+    mode: 'dark',
   },
-});
+})
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: (
-      <div className={"generate-room-container"}>
+      <div className={'generate-room-container'}>
         <GenerateRoom />
       </div>
     ),
   },
   {
-    path: "/:roomId",
+    path: '/:roomId',
     element: <AddUser />,
   },
   {
-    path: "/:userId/:roomId",
+    path: '/:userId/:roomId',
     element: (
       <SocketContext.Provider value={socket}>
         <App />
@@ -39,21 +39,21 @@ const router = createBrowserRouter([
     ),
   },
   {
-    path: "/:userId/:roomId?admin=true",
+    path: '/:userId/:roomId?admin=true',
     element: (
       <SocketContext.Provider value={socket}>
         <App />
       </SocketContext.Provider>
     ),
   },
-]);
+])
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={darkTheme}>
       <Box
-        sx={{ background: "rgb(20, 20, 40)", height: "100vh", width: "100vw" }}
+        sx={{ background: 'rgb(20, 20, 40)', height: '100vh', width: '100vw' }}
       >
         <Provider store={store}>
           <RouterProvider router={router} />
@@ -61,9 +61,9 @@ root.render(
       </Box>
     </ThemeProvider>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
